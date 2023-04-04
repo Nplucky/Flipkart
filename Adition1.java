39a946833f4d6107c8ae08e146bd3c935239e569
Nplucky <129033543+Nplucky@users.noreply.github.com>
2023-04-04 10:53:07 +0530

    Add files via upload

---
+++ Adition1.java
@@ -0,0 +1,15 @@
+class Adition1
+{
+	
+	public static void main(String[]args)
+	{
+       int a=10;
+	   int b=20;
+	   int c=(a+b);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+
+	}
+}
