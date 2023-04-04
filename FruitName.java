42a065b9751442720018733d4fcabd49b453f4a1
Nplucky <129033543+Nplucky@users.noreply.github.com>
2023-04-04 10:52:15 +0530

    Add files via upload

---
+++ FruitName.java
@@ -0,0 +1,12 @@
+class FruitName{
+
+    public static void main(String[]args){
+
+         System.out.println("Chiku");
+         System.out.println("Orange");
+         System.out.println("Lichu");
+         System.out.println("Banana");
+         System.out.println("Mango");
+
+     }
+}
